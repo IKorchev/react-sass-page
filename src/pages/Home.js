8c63de4e1489052588eb905066015d876e82fbb5
Korchev <Korchev94@gmail.com>
2021-04-05 20:48:20 +0100

    Lots of changes done, fetching data from Contentful and more..

---
+++ src/pages/Home.js
@@ -0,0 +1,45 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Image from "../assets/Me..jpg"
+import { Container, Row, Col } from "react-bootstrap"
+
+const Home = () => {
+
+
+  return (
+    <Container fluid className='m-0 p-0'>
+      <Navbar />
+      <Container id='main-content'>
+        <Row id='main-header' className='justify-content-center align-items-center'>
+          <Col lg={5}>
+            <Container className='text-center pt-5'>
+              <h1 className='text-dark mb-5'>Hai Anh</h1>
+              <Container
+                id='quotes'
+                className='d-flex flex-column justify-content-center'>
+                <figure className='d-flex flex-column justify-content-center'>
+                  <blockquote className='blockquote'>
+                    <p className='text-wrap p-0 m-0'>
+                      <i></i>"Earth" without "art" is just "eh".
+                    </p>
+                  </blockquote>
+                  <figcaption className='blockquote-footer d-flex justify-content-end'>
+                    <i>unknown</i>
+                  </figcaption>
+                </figure>
+              </Container>
+            </Container>
+            <Container className='d-flex justify-content-center'></Container>
+          </Col>
+          <Col lg={5} id='main-picture' className='main-column p-0'>
+            <img src={Image} width='100%' alt='Myself'></img>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </Container>
+  )
+}
+
+export default Home
